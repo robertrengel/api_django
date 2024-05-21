cebacbdea554593a15d45bb4a39d34a9a042051e
robertrengel <robertrengel@gmail.com>
2024-05-21 16:01:36 -0500

    api django complete with config render

--- project_api/settings.py
+++ project_api/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,15 +21,16 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-%=yb#ahp1e15i4vgh^i-llo*$9+28tg7%tvq)kq@+mgd7=rs6p"
+SECRET_KEY = os.environ.get("SECRET_KEY", default="your secret key")
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = "RENDER" not in os.environ
 
 ALLOWED_HOSTS = []
 
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Application definition
 
 INSTALLED_APPS = [
@@ -44,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -84,15 +88,24 @@ WSGI_APPLICATION = "project_api.wsgi.application"
 # }
 
 
+# DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.mysql",
+#        "NAME": "dbprueba",
+#        "USER": "prueba_api",
+#        "PASSWORD": "1234",
+#        "HOST": "localhost",  # O la dirección IP del servidor de la base de datos
+#        "PORT": "3306",  # El puerto de MySQL, normalmente 3306
+#    }
+# }
+
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "dbprueba",
-        "USER": "prueba_api",
-        "PASSWORD": "1234",
-        "HOST": "localhost",  # O la dirección IP del servidor de la base de datos
-        "PORT": "3306",  # El puerto de MySQL, normalmente 3306
-    }
+    "default": dj_database_url.config(
+        # aqui se coloca la base de datos por defecto
+        default="postgresql://postgres:postgres@localhost:5432/mysite",
+        conn_max_age=600,
+    )
 }
 
 
@@ -131,6 +144,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
